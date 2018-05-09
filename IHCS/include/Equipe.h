35117a8670aab9730128226ca0958af759b13ec2
Romain Donzé <romain.donze@he-arc.ch>
2018-05-09 14:29:25 +0200

    classe equipe

--- IHCS/include/Equipe.h
+++ IHCS/include/Equipe.h
@@ -1,16 +1,37 @@
 #ifndef EQUIPE_H
 #define EQUIPE_H
 
+#include <string>
+#include <list>
+
+#include "Joueur.h"
+
+using namespace std;
 
 class Equipe
 {
     public:
         Equipe();
+        Equipe(string nom, short _ID);
         virtual ~Equipe();
+
+        //méthodes
+        void Engager(Joueur &_joueur);
+        void Afficher();
+
+        //Attribus
+        string nom;
+        list<Joueur> listeJoueur;
+        short ID;
+
+        //getter
+        int getNiveauEquipe();
 
     protected:
 
     private:
+        //Attribus
+        int niveauEquipe;
 };
 
 #endif // EQUIPE_H
--- IHCS/src/Equipe.cpp
+++ IHCS/src/Equipe.cpp
@@ -1,11 +1,48 @@
 #include "Equipe.h"
+#include <iterator>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 Equipe::Equipe()
 {
-    //ctor
+    Equipe("Equipe", 0);
+}
+
+Equipe::Equipe(string _nom, short _ID)
+{
+    this->nom = _nom;
+    this->ID = _ID;
+    niveauEquipe=0;
 }
 
 Equipe::~Equipe()
 {
     //dtor
+}
+
+void Equipe::Engager(Joueur &_joueur)
+{
+    listeJoueur.push_back(_joueur);
+
+    niveauEquipe=(niveauEquipe+_joueur.getNiveauJoueur())/listeJoueur.size();
+}
+
+void Equipe::Afficher()
+{
+    cout << "--------------------------------------------------------"<< endl;
+    list <Joueur> :: iterator it;
+    cout << this->nom << " (Equipe " <<this->ID << ")" << "       Niveau d'equipe : "<< this->getNiveauEquipe() << endl;
+    cout << endl;
+    for(it = listeJoueur.begin(); it != listeJoueur.end(); ++it)
+    {
+        cout << "Nom du joueur : " << it->getNom() <<setfill(' ')<<setw(15)<< "   Niveau : " << it->getNiveauJoueur() << endl;
+    }
+    cout << '\n';
+    cout << "--------------------------------------------------------"<< endl;
+}
+
+int Equipe::getNiveauEquipe()
+{
+    return niveauEquipe;
 }
