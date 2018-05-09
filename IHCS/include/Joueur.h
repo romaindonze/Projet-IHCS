e755f04963d772c14a7280952125878b6f21453a
Romain Donzé <romain.donze@he-arc.ch>
2018-05-09 13:10:22 +0200

    ajout classe joueur

---
+++ IHCS/include/Joueur.h
@@ -0,0 +1,25 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include <string>
+
+using namespace std;
+
+
+class Joueur
+{
+    public:
+        Joueur();
+        Joueur(string _nom, int _niveauJoueur);
+        virtual ~Joueur();
+        //méthodes
+        void Afficher();
+    protected:
+
+    private:
+        string nom;
+        int NombreButs;
+        int niveauJoueur;
+};
+
+#endif // JOUEUR_H
---
+++ IHCS/src/Joueur.cpp
@@ -0,0 +1,22 @@
+#include "Joueur.h"
+#include <iostream>
+using namespace std;
+
+Joueur::Joueur()
+{
+    //ctor
+}
+Joueur::Joueur(string _nom, int niveau)
+{
+    //ctor
+}
+
+Joueur::~Joueur()
+{
+    //dtor
+}
+
+void Joueur::Afficher()
+{
+
+}
