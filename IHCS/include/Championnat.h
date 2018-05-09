7af9eead5e25b3615fbe8280752eaa024c1f427e
Mäder Alexandre <alexandr.mader@he-arc.ch>
2018-05-09 12:49:13 +0200

    ajout classe Championnat

---
+++ IHCS/include/Championnat.h
@@ -0,0 +1,16 @@
+#ifndef CHAMPIONNAT_H
+#define CHAMPIONNAT_H
+
+
+class Championnat
+{
+    public:
+        Championnat();
+        virtual ~Championnat();
+
+    protected:
+
+    private:
+};
+
+#endif // CHAMPIONNAT_H
---
+++ IHCS/src/Championnat.cpp
@@ -0,0 +1,11 @@
+#include "Championnat.h"
+
+Championnat::Championnat()
+{
+    //ctor
+}
+
+Championnat::~Championnat()
+{
+    //dtor
+}
